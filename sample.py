be4d1502366740b7fd07dc97d0f0ee4e031cfca9
harikarthickd583225148014-jpg <harikarthickd583225148014@nprcolleges.org>
2026-03-12 19:44:57 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,16 @@
+# My First GitHub Repository
+
+This is my first repository created as part of a GitHub learning activity.
+
+## About
+I created this repository to learn how to:
+- Create a GitHub account
+- Create a repository
+- Upload files
+- Share a repository link
+
+## Sample File
+This repository contains a simple Python program that prints a message.
+
+## Author
+Student learning GitHub
